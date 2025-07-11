387168c3d9430bcda29e941aa2245bfa270722ac
Timoniann <timoniann@gmail.com>
2025-07-11 19:03:24 +0300

    fix: update `FunctionComponent` and `ClassComponent` to use `JSX.Element` instead of `ElementValue`

--- src/jsx-runtime/types.ts
+++ src/jsx-runtime/types.ts
@@ -1,10 +1,10 @@
-import type { ElementValue } from "../index.ts"
+import { JSX } from "../index.ts"
 
-export type FunctionComponent<P = object> = (props: P) => ElementValue
+export type FunctionComponent<P = object> = (props: P) => JSX.Element
 
 export interface ClassComponent<P = object> {
   // biome-ignore lint/suspicious/noMisleadingInstantiator: Instantiator is needed for type checking
   new(props: P): ClassComponent
 
-  render(): ElementValue
+  render(): JSX.Element
 }
